58daef75769ddd40751a11a1f91680cc7018117b
stv-ekushida <e.kushida@st-ventures.jp>
2020-03-03 14:18:22 +0900

    Initial Commit

---
+++ ios-VNRecognizeTextRequest-demoTests/ios_VNRecognizeTextRequest_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_VNRecognizeTextRequest_demoTests.swift
+//  ios-VNRecognizeTextRequest-demoTests
+//
+//  Created by eiji kushida on 2020/03/03.
+//  Copyright © 2020 eiji kushida. All rights reserved.
+//
+
+import XCTest
+@testable import ios_VNRecognizeTextRequest_demo
+
+class ios_VNRecognizeTextRequest_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
